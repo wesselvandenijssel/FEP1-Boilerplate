3d57f95806ce82ee117ada004957c1dfeb12aed8
wesselvandenijssel <wessel069@gmail.com>
2024-09-06 17:11:41 +0200

    setup boilerplate code

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import prettier from "eslint-config-prettier";
+
+export default [
+	js.configs.all,
+	{
+		languageOptions: {
+			ecmaVersion: "latest",
+			globals: globals.browser,
+			sourceType: "module",
+		},
+		plugins: {
+			prettier,
+		},
+		rules: {
+			"no-console": "warn",
+		},
+	},
+];
